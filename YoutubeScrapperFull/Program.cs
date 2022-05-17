4db55b1ae9d5d97866393ab7e2e091fc3d7f444b
handshakingz@outlook.com <handshakingz@outlook.com>
2022-05-17 13:00:30 -0500

    search query yt scrapper

---
+++ YoutubeScrapperFull/Program.cs
@@ -0,0 +1,130 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace YoutubeScrapperFull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entrer youtube search query");
+            string searchQuery = Console.ReadLine();
+            Console.WriteLine("Enter no of videos to scrap");
+            string noOfVideos = Console.ReadLine();
+            Console.WriteLine("Press enter to start scrapping");
+            Console.ReadLine();
+
+            GetDataFromYoutubeAndFillCsv(searchQuery, noOfVideos);
+        }
+        public static List<string> GetListOfVideoUrls(IWebDriver driver, string searchQuery, string noOfVideos)
+        {
+            string searchQueryUrl = "https://www.youtube.com/results?search_query=" + searchQuery;
+            driver.Navigate().GoToUrl(searchQueryUrl);
+
+            
+            int a = 0;
+            while (a < int.Parse(noOfVideos) * 6)
+            {
+                driver.FindElement(By.TagName("body")).SendKeys(Keys.ArrowDown);
+                a++;
+                Thread.Sleep(new Random().Next(1,100));
+            }
+
+            driver.FindElement(By.TagName("body")).SendKeys(Keys.ArrowDown);
+
+            var elements = driver.FindElements(By.XPath("//*[@id='contents']/ytd-video-renderer")).ToList();
+            List<string> youtubeVideoUrlList = new List<string>();
+            int count = 0;
+            foreach (var element in elements)
+            {
+                var vidUrl = element.FindElement(By.XPath("div[1]/ytd-thumbnail/a")).GetAttribute("href");
+                youtubeVideoUrlList.Add(vidUrl);
+                count++;
+                if (count == int.Parse(noOfVideos)+3)
+                    break;
+            }
+            return youtubeVideoUrlList;
+        }
+        static public void GetDataFromYoutubeAndFillCsv(string searchQuery, string noOfVideos)
+        {
+            FileStream fileStream = new FileStream("youtube.csv", FileMode.Append);
+            StreamWriter writer = new StreamWriter(fileStream);
+            writer.WriteLine("SNo,Channel Name,Channel URL,Channel Views,Subscribers,Total Videos,Video Views,Email,Social Links");
+
+            IWebDriver chromeDriver = CreateChromeDriver();
+            IWebDriver firefoxDriver = CreateFirefoxDriver();
+            List<string> youtubeVideoUrlList = GetListOfVideoUrls(chromeDriver, searchQuery, noOfVideos);
+
+            int j = 2;
+            foreach (string vidUrl in youtubeVideoUrlList)
+            {
+                string videoUrl = vidUrl.Replace("\r", "");
+                List<string> channelData = new List<string>();
+                if (j % 2 == 0)
+                {
+                    channelData = Youtube.GetChannelData(videoUrl, firefoxDriver);
+                }
+                else
+                {
+                    channelData = Youtube.GetChannelData(videoUrl, chromeDriver);
+                }
+                if (channelData.Count > 0)
+                {
+                    writer.WriteLine(j - 1 + "," + channelData.ElementAt(0) + "," + channelData.ElementAt(1) + "," + channelData.ElementAt(2) + "," + channelData.ElementAt(3) + "," + channelData.ElementAt(4) + "," + channelData.ElementAt(5) + "," + "\"" + channelData.ElementAt(6) + "\"" + "," + "\"" + channelData.ElementAt(7).Trim() + "\"");
+                    writer.Flush();
+                    fileStream.Flush();
+
+                    Console.WriteLine(j - 1 + "\t" + channelData.ElementAt(0) + "\t" + channelData.ElementAt(6));
+                    //if (j > 4 && j % 5 == 0)
+                    //{
+                    //    Console.WriteLine("Intentially give a pause to do safe scraping");
+                    //    Thread.Sleep(random.Next(15, 25) * 1000);
+                    //}  
+                }
+                j++;
+            }
+            writer.Close();
+            fileStream.Close();
+
+            chromeDriver.Quit();
+            firefoxDriver.Quit();
+        }
+
+        static private ChromeDriver CreateChromeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--no-sandbox");
+            options.AddExcludedArgument("enable-automation");
+            //options.AddAdditionalCapability("useAutomationExtension", false);
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            //TimeSpan.FromSeconds is the max time for request to timeout
+            ChromeDriver driver = new ChromeDriver(service, options, TimeSpan.FromSeconds(80));
+            driver.Manage().Timeouts().PageLoad.Add(System.TimeSpan.FromSeconds(80));
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+        static private FirefoxDriver CreateFirefoxDriver()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--no-sandbox");
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            FirefoxDriver driver = new FirefoxDriver(service, options, TimeSpan.FromSeconds(80));
+            driver.Manage().Timeouts().PageLoad.Add(System.TimeSpan.FromSeconds(80));
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+    
+
+    }
+}
---
+++ YoutubeScrapperFull/Youtube.cs
@@ -0,0 +1,151 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace YoutubeScrapperFull
+{
+    class Youtube
+    {
+
+        static Random random = new Random();
+
+        public static List<string> GetChannelData(string videoUrl, IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl(videoUrl);
+            if (driver.Url != videoUrl)
+                return null;
+            //Thread.Sleep(random.Next(2, 4) * 1000);
+
+            string channelName = null;
+            string subscribers = null;
+            string channelUrl = null;
+            string totalVideos = null;
+            string totalVideoViews = null;
+            string description = null;
+
+            try
+            {
+                channelName = driver.FindElement(By.Id("text-container")).GetAttribute("innerText").Trim().Replace(",", "");
+            }
+            catch (Exception){}
+
+            try
+            {
+                subscribers = driver.FindElement(By.Id("owner-sub-count")).GetAttribute("innerText").Split(' ')[0].Trim().Replace(",", "");
+            }
+            catch (Exception){}
+
+
+            try
+            {          
+                channelUrl = driver.FindElement(By.XPath("//*[@id='top-row']/ytd-video-owner-renderer/a")).GetAttribute("href");
+            }
+            catch (Exception) { }
+
+            try
+            {
+                totalVideoViews = driver.FindElement(By.XPath("//*[@id='count']/ytd-video-view-count-renderer/span[1]")).GetAttribute("innerText").Replace(",", "").Split(' ')[0];
+            }
+            catch (Exception) { }
+
+            try
+            {
+                description = driver.FindElement(By.XPath("//*[@id='primary-inner']")).GetAttribute("innerText");
+            }
+            catch (Exception) { }
+
+
+            try
+            {
+                driver.Navigate().GoToUrl(channelUrl);
+                //Thread.Sleep(random.Next(2, 4) * 1000);
+                string allvideourl = driver.FindElement(By.XPath("//*[@id='play-button']/ytd-button-renderer/a")).GetAttribute("href");
+                driver.Navigate().GoToUrl(allvideourl);
+                //Thread.Sleep(random.Next(1, 4) * 1000); 
+            }
+            catch (Exception)
+            {
+                goto ABOUT;
+            }
+
+            try
+            {
+                totalVideos = driver.FindElement(By.XPath("//*[@id='publisher-container']/div/yt-formatted-string/span[3]")).GetAttribute("innerText").Replace(",", "");
+            }
+            catch (Exception) { }
+
+
+
+
+
+
+        ABOUT:
+            if (channelUrl == "")
+                return null;
+            string aboutUrl = channelUrl + "/about";
+            driver.Navigate().GoToUrl(aboutUrl);
+            //Thread.Sleep(random.Next(2, 4) * 1000);
+
+            string aboutPageData = null;
+            string totalChannelViews = null;
+            try
+            {
+                totalChannelViews = driver.FindElement(By.XPath("//*[@id='right-column']/yt-formatted-string[3]")).GetAttribute("innerText").Split(' ')[0].Replace(",","");
+            }
+            catch (Exception) { }
+            try
+            {
+                aboutPageData = driver.FindElement(By.XPath("//*[@id='left-column']")).GetAttribute("innerText");
+            }
+            catch (Exception) { }
+            
+
+            string email = StringManipulation.SearchEmail(description + " " + aboutPageData);
+            string sLinks = StringManipulation.SearchSocialLinks(description + " " + aboutPageData);
+
+            if(email == null || email == "")
+            {
+                email = SearchEmailFromGivenLinks(driver, sLinks);
+            }
+
+            List<string> channelData = new List<string>()
+            {
+                channelName,channelUrl,totalChannelViews,subscribers,totalVideos,totalVideoViews,email,sLinks
+            };
+            return channelData;
+        }
+
+       
+        public static string SearchEmailFromGivenLinks(IWebDriver driver, string sLinks)
+        {
+            string[] links = sLinks.Split('\n');
+            IJavaScriptExecutor scriptExecutor = (IJavaScriptExecutor)driver;
+            scriptExecutor.ExecuteScript("window.open()");
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            string email = "";
+            foreach(string link in links)
+            {
+                if (email == "" && !link.Contains("youtube.com"))
+                {
+                    try
+                    {
+                        driver.Navigate().GoToUrl(link);
+                        //Thread.Sleep(3000);
+                        string pageContent = driver.PageSource;
+                        email = StringManipulation.SearchEmail(pageContent);
+                    }
+                    catch (Exception) { }
+                }
+                else if(email != "")
+                {
+                    break;
+                }     
+            }
+            driver.Close();
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+            return email;
+        }
+
+    }
+}
